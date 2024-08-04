c88ff78f126dbf4d2c3dfa62cca298aa714bebec
LPdsgn <luigipedata.design@gmail.com>
2024-08-04 17:23:55 +0200

    Fixed shape divider "curve opacity" with file names and negative (flip) version

--- shapeDividers/customShapeDividers.php
+++ shapeDividers/customShapeDividers.php
@@ -12,8 +12,8 @@ function custom_elementor_shape_dividers( $additional_shapes ) {
 
 	$additional_shapes['curve-opacity'] = [
 		'title'        => esc_html__( 'Curve opacity', 'textdomain' ),
-		'url'          => get_stylesheet_directory_uri() . '/lp-elementor-widgets/shapeDividers/shape1.svg',
-		'path'         => get_stylesheet_directory() . '/lp-elementor-widgets/shapeDividers/shape1.svg',
+		'url'          => get_stylesheet_directory_uri() . '/lp-elementor-widgets/shapeDividers/curveOpacity.svg',
+		'path'         => get_stylesheet_directory() . '/lp-elementor-widgets/shapeDividers/curveOpacity.svg',
 		'has_flip'     => true,
 		'has_negative' => true,
 		'height_only'  => true,
